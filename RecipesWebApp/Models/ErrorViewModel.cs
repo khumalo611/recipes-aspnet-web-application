010e88e4842dd73352c2262a479d056023092962
khumalo611 <s224580434@mandela.ac.za>
2023-01-14 11:52:01 +0200

    Add project files.

---
+++ RecipesWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecipesWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
